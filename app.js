720952b0c2e90cfa37cf9b394a4a97f2852cf173
Rajat Gupta <55191777+rajatgupta24@users.noreply.github.com>
2020-06-24 21:33:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+let boxes = document.querySelectorAll(".box");
+const score = document.querySelector(".score");
+const time = document.querySelector(".time");
+
+boxes = Array.from(boxes);
+
+let result = 0;
+let currentTime = 60;
+let randomPos;
+let clickedPos;
+
+function setMole () {
+    for (let i = 0;i < boxes.length;i++){
+        boxes[i].classList.remove("mole");
+    }
+    randomPos = Math.floor(Math.random() * 9);
+    boxes[randomPos].classList.add("mole");
+}
+
+function check () {
+
+    if (clickedPos === randomPos){
+        result++;
+        score.textContent = result;
+    }else{
+        return;
+    }
+}
+
+function timer () {
+    if (currentTime <= 0){
+        clearInterval(interval);
+        return;
+    }
+    currentTime--;
+    time.textContent = currentTime;
+}
+
+for (let i = 0;i < boxes.length; i++){
+    boxes[i].addEventListener("click", () => {
+        clickedPos = i;
+        check();
+    });
+}
+
+let interval = setInterval (setMole , 750);
+let interval1 = setInterval (timer , 1000);
